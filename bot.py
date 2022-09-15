7bd4ac0722a741c5ed5404204ae3a100dd4de9b4
zlojKolhoznik <73634547+zlojKolhoznik@users.noreply.github.com>
2022-09-15 22:31:06 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,20 @@
+import telebot
+
+bot = telebot.TeleBot('5680668915:AAHN4u4Zm8VO4_dU7oRs1HtURCcPa4MPgGE')
+file_id = 'CgACAgIAAxkBAAMDYyNQYedGUE07ZxcUbj9dRfkzDB4AAmkNAAJpMQlIMYN_aB6-SjwpBA'
+
+# @bot.message_handler(content_types=['animation', 'video', 'photo', 'document'])
+# def get_file_id(msg: telebot.types.Message):
+# 	document_id = msg.document.file_id
+# 	print(document_id)
+
+
+@bot.message_handler(content_types=['new_chat_members'])
+def welcome(msg: telebot.types.Message):
+    bot.send_animation(msg.chat.id, file_id, reply_to_message_id=msg.message_id)
+    bot.reply_to(msg, 'Ти з конотопа?')
+
+
+if __name__=='__main__':
+    print('good')
+    bot.polling(non_stop=True)
